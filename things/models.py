f542e6e709a5b2a79a996873c499ffaced599f60
Fergan Yalim <ferganyalim13@gmail.com>
2022-10-05 23:40:25 +0100

    add constraints for the model field

--- things/models.py
+++ things/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 class Thing(models.Model):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.PositiveIntegerField()
+    name = models.CharField(max_length = 30, unique = True, blank = False)
+    description = models.CharField(max_length = 120, blank = True)
+    quantity = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
